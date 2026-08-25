7cc14291a2e90d1f0da6bf2c44dbbd2ee1e84c0b
danielamagdalenorojas-creator <daniela.magdalenorojas@gmail.com>
2026-08-24 21:20:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write("¡Hola, mundo!")
+st.title("Análisis de pases de fútbol")
+
+minuto = st.slider(
+    "Selecciona el minuto:",
+    min_value=0,
+    max_value=90,
+    value=45
+)
+
+st.write("Minuto seleccionado:", minuto)
